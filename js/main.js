7d603c1a1946a4d2bd22e0e9646a4e3d0a0a3632
Ve1okot <85491311+Ve1okot@users.noreply.github.com>
2021-08-20 14:51:45 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,6 +3,12 @@ $(document).ready(function () {
     var floorPath = $(".home-image path"); // каждый отдельный этаж в SVG
     var counterUp = $(".counter-up"); // кнопка увеличения этажа
     var counterDown = $(".counter-down"); // кнопка уменьшения этажа
+    var modal = $(".modal"); // модальное окно
+    var modalDialog = $(".modal-dialog"); // модальное окно в границах. Для определения клика за его пределами и закрытия этого окна
+    var modalCloseButton = $(".modal-close-button"); //кнопка закрытия модального окна
+    var viewFlatsButton = $(".view-flats"); //кнопка внизу "Смотреть квартиры на этаже"
+    var flatActive = $(".modal-image path"); //каждая отдельная квартира на картинке
+    var currentFlat = 2; //Переменная текущей подсвеченной квартиры
 
     // функция при наведении мышью на этаж
     floorPath.on('mouseover', function () {
@@ -10,6 +16,10 @@ $(document).ready(function () {
         currentFloor = $(this).attr("data-floor"); // получить значение текущего этажа
         $(".counter").text(currentFloor); //записать значение этажа в счётичик справа
     });
+
+    floorPath.on('click', toggleModal); //клик по этажу вызывает окно
+    modalCloseButton.on('click', toggleModal); //клик по крестику закрывает окно
+    viewFlatsButton.on("click", toggleModal); //клик по кнопке внизу открывает модальное окно
 
     //отслеживание клика по кнопке вверх
     counterUp.on("click", function () {
@@ -35,5 +45,18 @@ $(document).ready(function () {
         floorPath.removeClass("current-floor"); //удалить активный класс у всех этажей
         $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor"); //подсветка текущего этажа
         }
+    });
+
+    //функция переключения класса видимости для модального окна
+    function toggleModal () {
+        modal.toggleClass('is-open');
+    };
+
+    // функция при наведении мышью на квартиру
+    flatActive.on('mouseover', function () {
+        flatActive.removeClass("flat-link-active"); //удалить активный класс у всех квартир
+        currentFlat = $(this).attr("flat-link-active"); // получить значение текущей квартиры
+        $(`[data-flat=${usFlatActive}]`).toggleClass("flat-link-active");
+        
     });
 });
